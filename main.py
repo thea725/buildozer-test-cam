9d9032062f1f0f47c2cb499266684cd5bfd62c09
M Rifki Muchtar <91528096+thea725@users.noreply.github.com>
2023-12-12 16:53:49 +0700

    process02

--- main.py
+++ main.py
@@ -124,23 +124,16 @@ class AndroidCamera(BoxLayout):
     def update_texture(self, instance, texture):
         frame = self.frame_from_buf()
 
-        # Additional processing or modification of the frame can be done here
-
-        frame = self.frame_to_screen(frame)
-
         # Update the camera texture
         texture.blit_buffer(frame.tostring(), colorfmt='rgb', bufferfmt='ubyte')
 
     def frame_from_buf(self):
         w, h = self.camera.resolution
         frame = np.frombuffer(self.camera._camera._buffer.tostring(), dtype='uint8').reshape((h + h // 2, w))
-        frame_bgr = cv2.cvtColor(frame, cv2.COLOR_YUV2BGR_NV21)
-        return np.rot90(frame_bgr, 3)
-
-    def frame_to_screen(self, frame):
         enhance = normalization(frame)
         result = edge_detection(frame, enhance)
-        result
+        frame_bgr = cv2.cvtColor(result, cv2.COLOR_YUV2BGR_NV21)
+        return frame_bgr
 
 class MyApp(App):
     def build(self):
