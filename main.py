25333f0de9add5587d93087d00bbc2fd27efe0c3
M Rifki Muchtar <91528096+thea725@users.noreply.github.com>
2023-12-13 09:59:12 +0700

    test minimum cam

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.image import Image
-from kivy.clock import Clock
-from kivy.graphics.texture import Texture
-import cv2
+from kivy.uix.camera import Camera
 
 class CameraApp(App):
     def build(self):
@@ -11,32 +8,10 @@ class CameraApp(App):
         layout = BoxLayout(orientation='vertical')
 
         # Menambahkan objek kamera
-        self.capture = cv2.VideoCapture(0)
-        self.image = Image()
-        layout.add_widget(self.image)
-
-        # Mendaftarkan fungsi update untuk mengambil frame kamera secara berkala
-        Clock.schedule_interval(self.update, 1.0 / 30.0)
+        self.camera = Camera(resolution=(640, 480), play=True)
+        layout.add_widget(self.camera)
 
         return layout
-
-    def update(self, dt):
-        # Membaca frame dari kamera
-        ret, frame = self.capture.read()
-
-        if ret:
-            # Mengonversi frame menjadi grayscale
-            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-            # Mengubah format frame menjadi RGB
-            rgb_frame = cv2.cvtColor(gray_frame, cv2.COLOR_GRAY2RGB)
-
-            # Membuat objek texture Kivy dari frame
-            texture = Texture.create(size=(rgb_frame.shape[1], rgb_frame.shape[0]), colorfmt='rgb')
-            texture.blit_buffer(rgb_frame.tostring(), colorfmt='rgb', bufferfmt='ubyte')
-
-            # Menampilkan frame di widget gambar
-            self.image.texture = texture
 
 if __name__ == '__main__':
     CameraApp().run()
