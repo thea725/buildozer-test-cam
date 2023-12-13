fe15130502a76ea8cfbdb03dddebee7cdd2242ef
M Rifki Muchtar <91528096+thea725@users.noreply.github.com>
2023-12-13 10:59:59 +0700

    test cam to canvas

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.camera import Camera
 from kivy.uix.image import Image
+from kivy.clock import Clock
 
 import cv2
-import numpy as np
-from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 
 class CameraApp(App):
@@ -17,46 +16,34 @@ class CameraApp(App):
         self.camera = Camera(resolution=(640, 480), play=True)
         layout.add_widget(self.camera)
 
-        # Menambahkan objek gambar untuk menampilkan hasil olahan gambar
+        # Menambahkan objek tampilan gambar untuk menampilkan hasil pemrosesan OpenCV
         self.image = Image()
         layout.add_widget(self.image)
 
-        # Menjadwalkan pemanggilan fungsi update_frame setiap frame
-        Clock.schedule_interval(self.update_frame, 1.0 / 30.0)
+        # Menjadwalkan fungsi update setiap frame kamera
+        Clock.schedule_interval(self.update, 1.0 / 30.0)
 
         return layout
 
-    def update_frame(self, dt):
-        # Mengambil frame dari kamera
+    def update(self, dt):
+        # Mendapatkan frame dari kamera
         frame = self.camera.texture
-
-        # Mengonversi frame ke skala abu-abu menggunakan OpenCV
         if frame is not None:
-            frame = self.convert_to_grayscale(frame)
+            # Mengubah frame ke format yang dapat diolah oleh OpenCV
+            frame_data = frame.pixels
+            img = cv2.cvtColor(frame_data, cv2.COLOR_RGBA2BGR)
 
-            # Menyusun frame hasil olahan untuk ditampilkan di objek gambar
-            self.image.texture = frame
+            # Mengonversi gambar ke grayscale
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    def convert_to_grayscale(self, frame):
-        # Mengambil data dari texture
-        buf = frame.pixels
+            # Mengubah kembali ke format yang dapat ditampilkan oleh Kivy
+            buf1 = cv2.flip(gray, 0)
+            buf = buf1.tobytes()
 
-        # Mengonversi data ke array NumPy
-        img = cv2.flip(
-            cv2.cvtColor(
-                np.frombuffer(buf, dtype=np.uint8).reshape((frame.height, frame.width, 4)),
-                cv2.COLOR_RGBA2BGR
-            ),
-            0
-        )
-
-        # Mengonversi frame ke skala abu-abu
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        # Mengonversi kembali ke format RGBA
-        buf[:] = cv2.cvtColor(gray, cv2.COLOR_GRAY2RGBA).flatten()
-
-        return frame
+            # Menampilkan gambar hasil pemrosesan
+            texture = Texture.create(size=(gray.shape[1], gray.shape[0]), colorfmt='luminance')
+            texture.blit_buffer(buf, colorfmt='luminance', bufferfmt='ubyte')
+            self.image.texture = texture
 
 if __name__ == '__main__':
     CameraApp().run()
