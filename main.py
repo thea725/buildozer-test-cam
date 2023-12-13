b283d2de275ca039ed0c5a282ba5cc532991f0bc
M Rifki Muchtar <91528096+thea725@users.noreply.github.com>
2023-12-13 17:04:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,39 @@
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+import kivy
+try:
+    import cv2
+except:
+    from cv import cv2
+import numpy as np
 from kivy.uix.camera import Camera
-from kivy.uix.image import Image
-from kivy.clock import Clock
-
-import cv2
 from kivy.graphics.texture import Texture
 
-class CameraApp(App):
-    def build(self):
-        # Membuat tata letak utama
-        layout = BoxLayout(orientation='vertical')
+class Camera2(Camera):
+    firstFrame=None
+    def _camera_loaded(self, *largs):
+        if kivy.platform=='android':
+            self.texture = Texture.create(size=self.resolution,colorfmt='rgb')
+            self.texture_size = list(self.texture.size)
+        else:
+            super(Camera2, self)._camera_loaded()
 
-        # Menambahkan objek kamera
-        self.camera = Camera(resolution=(640, 480), play=True)
-        layout.add_widget(self.camera)
+    def on_tex(self, *l):
+        if kivy.platform=='android':
+            buf = self._camera.grab_frame()
+            if not buf:
+                return
+            frame = self._camera.decode_frame(buf)
+            self.image = frame = self.process_frame(frame)
+            buf = frame.tostring()
+            self.texture.blit_buffer(buf, colorfmt='rgb', bufferfmt='ubyte')
+        super(Camera2, self).on_tex(*l)
 
-        # Menjadwalkan fungsi update setiap frame kamera
-        self.camera.bind(on_tex=self.update)
-
-        return layout
-
-    def update(self, dt):
-        # Mendapatkan frame dari kamera
-        frame = self.camera.texture
-
-        if frame is None:
-            frame = cv2.imread("/data/data/org.test.recycleai/files/app/image.jpg")
-            
-        if frame is not None:
-            # Mengubah frame ke format yang dapat diolah oleh OpenCV
-            frame_data = frame.pixels
-            img = cv2.cvtColor(frame_data, cv2.COLOR_RGBA2BGR)
-
-            # Mengonversi gambar ke grayscale
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-            # Mengubah kembali ke format yang dapat ditampilkan oleh Kivy
-            buf1 = cv2.flip(gray, 0)
-            buf = buf1.tobytes()
-
-            # Menampilkan gambar hasil pemrosesan
-            texture = Texture.create(size=(gray.shape[1], gray.shape[0]), colorfmt='luminance')
-            texture.blit_buffer(buf, colorfmt='luminance', bufferfmt='ubyte')
-            self.camera.texture = texture
-
-if __name__ == '__main__':
-    CameraApp().run()
+    def process_frame(self,frame):
+        r,g,b=cv2.split(frame)
+        frame=cv2.merge((b,g,r))        
+        rows,cols,channel=frame.shape
+        M=cv2.getRotationMatrix2D((cols/2,rows/2),90,1)
+        dst=cv2.warpAffine(frame,M,(cols,rows))
+        frame=cv2.flip(dst,1)
+        if self.index==1:
+            frame=cv2.flip(dst,-1)
+        return frame
