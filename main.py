46c91c4be7c6d1732cb1dffdd05382fd9fc62e15
M Rifki Muchtar <91528096+thea725@users.noreply.github.com>
2023-12-13 09:58:40 +0700

    opencv test

--- main.py
+++ main.py
@@ -1,55 +1,42 @@
-import android
-import android.activity
-from os import remove
-from jnius import autoclass, cast
-from plyer.facades import Camera
-from plyer.platforms.android import activity
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+import cv2
 
-Intent = autoclass('android.content.Intent')
-PythonActivity = autoclass('org.kivy.android.PythonActivity')
-MediaStore = autoclass('android.provider.MediaStore')
-Uri = autoclass('android.net.Uri')
+class CameraApp(App):
+    def build(self):
+        # Membuat tata letak utama
+        layout = BoxLayout(orientation='vertical')
 
+        # Menambahkan objek kamera
+        self.capture = cv2.VideoCapture(0)
+        self.image = Image()
+        layout.add_widget(self.image)
 
-class AndroidCamera(Camera):
+        # Mendaftarkan fungsi update untuk mengambil frame kamera secara berkala
+        Clock.schedule_interval(self.update, 1.0 / 30.0)
 
-    def _take_picture(self, on_complete, filename=None):
-        assert on_complete is not None
-        self.on_complete = on_complete
-        self.filename = filename
-        android.activity.unbind(on_activity_result=self._on_activity_result)
-        android.activity.bind(on_activity_result=self._on_activity_result)
-        intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        uri = Uri.parse('file://' + filename)
-        parcelable = cast('android.os.Parcelable', uri)
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, parcelable)
-        activity.startActivityForResult(intent, 0x123)
+        return layout
 
-    def _take_video(self, on_complete, filename=None):
-        assert on_complete is not None
-        self.on_complete = on_complete
-        self.filename = filename
-        android.activity.unbind(on_activity_result=self._on_activity_result)
-        android.activity.bind(on_activity_result=self._on_activity_result)
-        intent = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
-        uri = Uri.parse('file://' + filename)
-        parcelable = cast('android.os.Parcelable', uri)
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, parcelable)
+    def update(self, dt):
+        # Membaca frame dari kamera
+        ret, frame = self.capture.read()
 
-        # 0 = low quality, suitable for MMS messages,
-        # 1 = high quality
-        intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1)
-        activity.startActivityForResult(intent, 0x123)
+        if ret:
+            # Mengonversi frame menjadi grayscale
+            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    def _on_activity_result(self, requestCode, resultCode, intent):
-        if requestCode != 0x123:
-            return
-        android.activity.unbind(on_activity_result=self._on_activity_result)
-        if self.on_complete(self.filename):
-            self._remove(self.filename)
+            # Mengubah format frame menjadi RGB
+            rgb_frame = cv2.cvtColor(gray_frame, cv2.COLOR_GRAY2RGB)
 
-    def _remove(self, fn):
-        try:
-            remove(fn)
-        except OSError:
-            pass
+            # Membuat objek texture Kivy dari frame
+            texture = Texture.create(size=(rgb_frame.shape[1], rgb_frame.shape[0]), colorfmt='rgb')
+            texture.blit_buffer(rgb_frame.tostring(), colorfmt='rgb', bufferfmt='ubyte')
+
+            # Menampilkan frame di widget gambar
+            self.image.texture = texture
+
+if __name__ == '__main__':
+    CameraApp().run()
