bb568ad84cb462137414b14eb0916582e4f96f55
M Rifki Muchtar <91528096+thea725@users.noreply.github.com>
2023-12-14 15:04:28 +0700

    semireroll

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
-import cv2
 from kivy.app import App
-from kivy.uix.image import Image
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.camera import Camera
 from kivy.graphics.texture import Texture
-from kivy.clock import Clock
+import cv2
+import numpy as np
 
-class CameraApp(App):
+class AndroidCamera(BoxLayout):
+    def __init__(self, **kwargs):
+        super(AndroidCamera, self).__init__(**kwargs)
+
+        self.camera = Camera(play=True)
+        self.camera.bind(on_tex=self.update_texture)
+        self.add_widget(self.camera)
+
+    def update_texture(self, instance, texture):
+        frame = self.frame_from_buf()
+
+        # Additional processing or modification of the frame can be done here
+
+        self.frame_to_screen(frame)
+
+        # Update the camera texture
+        texture.blit_buffer(frame.tostring(), colorfmt='rgb', bufferfmt='ubyte')
+
+    def frame_from_buf(self):
+        w, h = self.camera.resolution
+        frame = np.frombuffer(self.camera._camera._buffer.tostring(), dtype='uint8').reshape((h + h // 2, w))
+        frame_bgr = cv2.cvtColor(frame, cv2.COLOR_YUV2BGR_NV21)
+        return np.rot90(frame_bgr, 3)
+
+    def frame_to_screen(self, frame):
+        # Additional processing or modification of the frame can be done here
+
+        # Example: Display the frame directly without additional processing
+        pass
+
+class MyApp(App):
     def build(self):
-        self.capture = cv2.VideoCapture(0,cv2.CAP_ANDROID)
-        self.img = Image()
-
-        Clock.schedule_interval(self.update, 1.0 / 30.0)
-
-        return self.img
-
-    def update(self, dt):
-        ret, frame = self.capture.read()
-        self.img.texture = self.texture(frame)
-
-    def texture(self, frame):
-        buf1 = cv2.flip(frame, 0)
-        buf = buf1.tostring()
-        image_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
-        image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
-        return image_texture
+        return AndroidCamera()
 
 if __name__ == '__main__':
-    CameraApp().run()
+    MyApp().run()
