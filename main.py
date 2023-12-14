ea89a46e7c7cff35ec6e8c9f1d81e1ec9e0c0a1c
M Rifki Muchtar <91528096+thea725@users.noreply.github.com>
2023-12-14 12:28:21 +0700

    test

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
-#coding=utf-8
-#qpy:kivy
-
-import time
+import kivy
 from kivy.app import App
-from kivy.clock import Clock
-from kivy.uix.boxlayout import BoxLayout
-from camera import Camera2
-
-class MyLayout(BoxLayout):
-    pass
-
-class MainApp(App):
+from kivy.uix.label import Label
+import cv2
+import numpy as np
+from multiprocessing import Process
+  
+class MyLabelApp(App):
     def build(self):
-        return MyLayout()
-    def on_start(self):
-        Clock.schedule_once(self.detect,5)
-
-    def detect(self,nap):
-        image=self.root.ids.camera.image
-        rows,cols=image.shape[:2]
-        ctime=time.ctime()[11:19]
-        self.root.ids.label.text='%s image rows:%d cols:%d'%(ctime,rows,cols)
-        Clock.schedule_once(self.detect,1)
-
-if __name__ == '__main__':
-    MainApp().run()
+        caption = cv2.VideoCapture(0,cv2.CAP_V4L2)
+        if not caption.isOpened():
+            lbl2 = Label(text ="cannot open camera 0")
+            return lbl2
+            caption.release()
+        else:
+            caption.release()
+        caption1 = cv2.VideoCapture(1,cv2.CAP_V4L2)
+        if not caption1.isOpened():
+            lbl3 = Label(text ="cannot open camera 1")
+            return lbl3
+            caption1.release()
+        else:
+            caption1.release()
+        lbl = Label(text ="Android Two Cam Is running, Close window and save ouput.avi locally!!! ")
+        return lbl
+def cap():
+    cap = cv2.VideoCapture(0,cv2.CAP_V4L2)
+    cap1 = cv2.VideoCapture(1,cv2.CAP_V4L2)
+    out = cv2.VideoWriter('/storage/emulated/0/dcim/output.avi',cv2.VideoWriter_fourcc(*'XVID'), cap.get(cv2.CAP_PROP_FPS), (1440,720))
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        ret1, frame1 = cap1.read()
+        if ret == True:
+            reframe = cv2.resize(frame,(720, 720), interpolation = cv2.INTER_AREA)
+            reframe1 = cv2.resize(frame1,(720, 720), interpolation = cv2.INTER_AREA)
+            both = np.column_stack((reframe, reframe1))
+            out.write(both) 
+if __name__ == "__main__":
+    p1 = Process(target=cap)
+    p1.start()
+    label = MyLabelApp()
+    label.run()
+    label.on_stop(p1.terminate())
