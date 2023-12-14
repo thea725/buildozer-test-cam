5f8050c3c37963d61df91be3f8d5a0d6f01e0daf
M Rifki Muchtar <91528096+thea725@users.noreply.github.com>
2023-12-14 16:41:26 +0700

    dualwidget

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.camera import Camera
+from kivy.uix.image import Image
 from kivy.graphics.texture import Texture
 import cv2
 import numpy as np
@@ -9,16 +10,22 @@ class AndroidCamera(BoxLayout):
     def __init__(self, **kwargs):
         super(AndroidCamera, self).__init__(**kwargs)
 
+        # Camera Widget
         self.camera = Camera(play=True)
         self.camera.bind(on_tex=self.update_texture)
         self.add_widget(self.camera)
+
+        # Image Widget for displaying processed grayscale image
+        self.image_widget = Image()
+        self.add_widget(self.image_widget)
 
     def update_texture(self, instance, texture):
         frame = self.frame_from_buf()
 
         # Additional processing or modification of the frame can be done here
+        grayscale_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        self.frame_to_screen(frame)
+        self.frame_to_screen(grayscale_frame)
 
         # Update the camera texture
         texture.blit_buffer(frame.tostring(), colorfmt='rgb', bufferfmt='ubyte')
@@ -32,8 +39,10 @@ class AndroidCamera(BoxLayout):
     def frame_to_screen(self, frame):
         # Additional processing or modification of the frame can be done here
 
-        # Example: Display the frame directly without additional processing
-        pass
+        # Display the processed frame in the Image widget
+        texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='luminance')
+        texture.blit_buffer(frame.tobytes(), colorfmt='luminance', bufferfmt='ubyte')
+        self.image_widget.texture = texture
 
 class MyApp(App):
     def build(self):
