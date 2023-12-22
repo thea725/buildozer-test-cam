1dae1650fb4021fc2014737814bdd34b3671c607
M Rifki Muchtar <91528096+thea725@users.noreply.github.com>
2023-12-22 14:53:23 +0700

    hello world

--- main.py
+++ main.py
@@ -1,47 +1,9 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.camera import Camera
-from kivy.graphics.texture import Texture
-import cv2
-import numpy as np
-from android.permissions import request_permissions, Permission
-request_permissions([Permission.CAMERA])
-__all__ = ('Camera', )
+from kivy.uix.label import Label
 
-
-class AndroidCamera(BoxLayout):
-    def __init__(self, **kwargs):
-        super(AndroidCamera, self).__init__(**kwargs)
-
-        self.camera = Camera(play=True)
-        self.camera.bind(on_tex=self.update_texture)
-        self.add_widget(self.camera)
-
-    def update_texture(self, instance, texture):
-        frame = self.frame_from_buf()
-
-        # Additional processing or modification of the frame can be done here
-
-        self.frame_to_screen(frame)
-
-        # Update the camera texture
-        texture.blit_buffer(frame.tostring(), colorfmt='rgb', bufferfmt='ubyte')
-
-    def frame_from_buf(self):
-        w, h = self.camera.resolution
-        frame = np.frombuffer(self.camera._camera._buffer.tostring(), dtype='uint8').reshape((h + h // 2, w))
-        frame_bgr = cv2.cvtColor(frame, cv2.COLOR_YUV2BGR_NV21)
-        return np.rot90(frame_bgr, 3)
-
-    def frame_to_screen(self, frame):
-        # Additional processing or modification of the frame can be done here
-
-        # Example: Display the frame directly without additional processing
-        pass
-
-class MyApp(App):
+class HelloWorldApp(App):
     def build(self):
-        return AndroidCamera()
+        return Label(text='Hello, World!')
 
 if __name__ == '__main__':
-    MyApp().run()
+    HelloWorldApp().run()
